730ac6af593f970c070e2c46cb348ed088a07a0c
Druthvik7 <s.druthvik@gmail.com>
2024-07-20 12:47:02 +0530

    c

--- build_files.sh
+++ build_files.sh
@@ -2,5 +2,5 @@ echo " BUILD START"
 python3.9 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput --clear
 echo "BUILD END"
